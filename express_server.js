200b5c31f0eb251de20f2f31215e3f9a424559c1
parthjd <parthjd28@gmail.com>
2020-01-20 20:37:48 +0000

    EJS templating engine to render web pages.

--- express_server.js
+++ express_server.js
@@ -8,16 +8,30 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+
+  res.render("urls_index", templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body> Hello <b> World </b> </body></html>\n");
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase /* What goes here? */
+  };
+  res.render("urls_show", templateVars);
 });
+
+// app.get("/", (req, res) => {
+//   res.send("Hello");
+// });
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body> Hello <b> World </b> </body></html>\n");
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
