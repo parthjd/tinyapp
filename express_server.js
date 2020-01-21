5688184368883565687874ec4aa942f0afc9211d
PJD <parthjd28@gmail.com>
2020-01-21 12:21:56 -0500

    "added new link"

--- express_server.js
+++ express_server.js
@@ -7,32 +7,50 @@ const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
-
   res.render("urls_index", templateVars);
+});
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
     shortURL: req.params.shortURL,
-    longURL: urlDatabase /* What goes here? */
+    longURL: urlDatabase[req.params.shortURL]
   };
   res.render("urls_show", templateVars);
 });
 
-// app.get("/", (req, res) => {
-//   res.send("Hello");
-// });
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
-
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body> Hello <b> World </b> </body></html>\n");
-// });
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+app.post("/urls", (req, res) => {
+  let longURL = req.body.longURL;
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = longURL;
+  res.redirect("/urls/" + shortURL);
 });
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body> Hello <b> World </b> </body></html>\n");
+});
+
+function generateRandomString() {
+  let randomString = Math.random()
+    .toString(36)
+    .substring(2, 8);
+  return randomString;
+}
