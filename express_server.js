3decc48223b084d3a1ba20a9e8c66f4afe87198e
PJD <parthjd28@gmail.com>
2020-01-21 13:27:23 -0500

    added delete buttons

--- express_server.js
+++ express_server.js
@@ -37,16 +37,22 @@ app.post("/urls", (req, res) => {
   res.redirect("/urls/" + shortURL);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.post("/urls/:shortURL/delete", (req, res) => {
+  let shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect("/urls");
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body> Hello <b> World </b> </body></html>\n");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello");
+// });
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body> Hello <b> World </b> </body></html>\n");
+// });
 
 function generateRandomString() {
   let randomString = Math.random()
