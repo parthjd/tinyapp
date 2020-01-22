43b1ae98fe6e98d21e36b6dac998bed75fd27d7c
PJD <parthjd28@gmail.com>
2020-01-22 13:58:20 -0500

    added the login and logout option

--- express_server.js
+++ express_server.js
@@ -1,3 +1,5 @@
+//  ******* Variable declaration and require ******
+
 const express = require("express");
 const app = express();
 const PORT = 8080;
@@ -12,10 +14,17 @@ const urlDatabase = {
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//  *** urls homepage ***
+
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
+
+//  *** urls  create a new url ***
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
@@ -24,7 +33,8 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
     shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL]
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
   };
   res.render("urls_show", templateVars);
 });
@@ -36,31 +46,42 @@ app.post("/urls", (req, res) => {
   res.redirect("/urls/" + shortURL);
 });
 
+// *** delete  URL ***
+
 app.post("/urls/:shortURL/delete", (req, res) => {
   let shortURL = req.params.shortURL;
   delete urlDatabase[shortURL];
   res.redirect("/urls");
 });
 
+// *** Update URL ***
+
 app.post("/urls/:shortURL", (req, res) => {
   urlDatabase[req.params.shortURL] = req.body.longURL;
   res.redirect("/urls");
 });
+
+// *** Login & Logout ***
+
 app.post("/login", (req, res) => {
-  res.cookie("username", req.body.username);
+  let username = req.body.username;
+  res.cookie("username", username);
   res.redirect("/urls");
 });
 
-// app.get("/", (req, res) => {
-//   res.send("Hello");
-// });
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
+app.post("/logout", (req, res) => {
+  let username = req.body.username;
+  res.clearCookie("username", username);
+  res.redirect("/urls");
+});
+
+// app.get("/login", (req, res) => {
+//   // let username = req.body.username;
+//   // res.cookie("username", username);
+//   res.redirect("/urls");
 // });
 
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body> Hello <b> World </b> </body></html>\n");
-// });
+//  ***function to generate random string***
 
 function generateRandomString() {
   let randomString = Math.random()
